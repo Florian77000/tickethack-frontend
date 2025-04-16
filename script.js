30b7e90ff4d9675818ef36c149bf15fc1cfa49cb
Florian <floriandelaunay75@gmail.com>
2025-04-16 17:15:46 +0200

    reinit div au click btn

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.querySelector("#search-btn").addEventListener("click", function () {
+  document.querySelector("#right-container").innerHTML = "";
   const trip = {
     departure: document.querySelector("#DepartureInput").value,
     arrival: document.querySelector("#ArrivalInput").value,
     date: document.querySelector("#dateInput").value,
   };
+
   fetch("http://localhost:3000/trips/search", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -12,21 +14,24 @@ document.querySelector("#search-btn").addEventListener("click", function () {
     .then((response) => response.json())
     .then((data) => {
       if (data.dataTrip.length != 0) {
-        document.querySelector("#timeToBook").style.display = "none";
-        document.querySelector("#noTrip").style.display = "none";
         for (let i = 0; i < data.dataTrip.length; i++) {
           let tripOk = `
                 <div class="row">
-                    <div class="pointApointB">${data.dataTrip[i].departure} > ${data.dataTrip[i].arrival}</div>
-                    <div class="departureTime">${moment(data.dataTrip[i].date).format("HH:mm")}</div>
+                    <div class="pointApointB">${data.dataTrip[i].departure} > ${
+            data.dataTrip[i].arrival
+          }</div>
+                    <div class="departureTime">${moment(
+                      data.dataTrip[i].date
+                    ).format("HH:mm")}</div>
                     <div class="tripPrice">${data.dataTrip[i].price} €</div>
-                    <button type='button' class="addtoCart" id="${data.dataTrip[i]._id}">Book</button>
+                    <button type='button' class="addtoCart" id="${
+                      data.dataTrip[i]._id
+                    }">Book</button>
                 </div>
                 `;
           document.querySelector("#right-container").innerHTML += tripOk;
         }
       } else {
-        document.querySelector("#timeToBook").style.display = "none";
         const noTrip = `
                 <div id="noTrip">
                     <img class="logo" src="images/notfound.png" />
@@ -36,7 +41,8 @@ document.querySelector("#search-btn").addEventListener("click", function () {
         document.querySelector("#right-container").innerHTML += noTrip;
       }
     })
-    .then(() => {      for (let j = 0; j < document.querySelectorAll(".addtoCart").length; j++) {
+    .then(() => {
+      for (let j = 0; j < document.querySelectorAll(".addtoCart").length; j++) {
         document
           .querySelectorAll(".addtoCart")
           [j].addEventListener("click", function () {
@@ -45,4 +51,5 @@ document.querySelector("#search-btn").addEventListener("click", function () {
       }
     });
 });
- //version4
+
+//version4
