61517b1c366c80ebc54425701f7f378ea5f2c7a5
Your nameMarion <marion.rdx@gmail.com>
2025-04-16 13:46:43 +0200

    font Roboto

--- script.js
+++ script.js
@@ -13,21 +13,17 @@ document.querySelector("#search-btn").addEventListener("click", function () {
     .then((data) => {
       console.log(data);
       if (data.dataTrip.length != 0) {
-        document.querySelector("#right-container").style.display = "none";
+        document.querySelector("#timeToBook").style.display = "none";
         for (let i = 0; i < data.dataTrip.length; i++) {
           let tripOk = `
                 <div class="row">
-                    <div class="pointApointB">${data.dataTrip[i].departure} > ${
-            data.dataTrip[i].arrival
-          }</div>
-                    <div class="departureTime">${moment(
-                      data.dataTrip[i].date
-                    ).format("HH:mm")}</div>
+                    <div class="pointApointB">${data.dataTrip[i].departure} > ${data.dataTrip[i].arrival}</div>
+                    <div class="departureTime">${moment(data.dataTrip[i].date).format("HH:mm")}</div>
                     <div class="tripPrice">${data.dataTrip[i].price} €</div>
                     <button class="addtoCart">Book</button>
                 </div>
                 `;
-          document.querySelector("#newright-container").innerHTML += tripOk;
+          document.querySelector("#right-container").innerHTML += tripOk;
         }
       } else {
         const noTrip = `
